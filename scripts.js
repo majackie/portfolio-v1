bafd9abc7924f6f7ad33bb8d956f6660b3428599
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-06-03 14:21:08 -0700

    add responsive navbar and skeleton

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
-function hamburgerMenu() {
-  const home = document.getElementById("home");
+function openMenu() {
+  const home = document.getElementById("navContainer");
   if (home.className === "nav") {
     home.className = "nav vertical";
   } else {
     home.className = "nav";
   }
+}
+
+function closeMenu() {
+  const home = document.getElementById("navContainer");
+  home.className = "nav";
 }
